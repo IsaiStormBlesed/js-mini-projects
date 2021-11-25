10998372aba185619fc032ed5694ce8005731fac
Jose Isai Reyes Hernandez <79279643+IsaiStormBlesed@users.noreply.github.com>
2021-11-25 11:37:50 -0600

    Add files via upload
    Little ripple effect using Vanilla JS

---
+++ btn-ripple/script.js
@@ -0,0 +1,26 @@
+//e.clientX and e.clientY
+//e.target.offsetTop and e.target.offsetLeft
+
+const btn = document.querySelector('.ripple')
+
+
+btn.addEventListener('click', (e) => {
+
+	const x = e.clientX
+	const y = e.clientY
+
+	const btnTop = e.target.offsetTop
+	const btnLeft = e.target.offsetLeft
+
+	const xInside = x - btnLeft
+	const yInside = y - btnTop
+
+	const circle = document.createElement('span')
+	circle.classList.add('circle')
+	circle.style.top = yInside + 'px'
+	circle.style.left = xInside + 'px'
+
+	btn.appendChild(circle)
+
+	setTimeout(() => circle.remove(), 500)
+})
